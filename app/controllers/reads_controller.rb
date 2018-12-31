2d837aad5a13dd1b3b7d050a6a4539e8ceb38b37
thuhien191197 <thuhien191197@gmail.com>
2019-01-01 04:24:10 +0700

    first doc online

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -3,15 +3,12 @@ class ReadsController < ApplicationController
 	include SessionsHelper
     before_action :require_admin, only:[:new,:create,:edit, :update, :destroy]
   	before_action :find_book, only:[:show, :edit, :update, :destroy]
-	  before_action :require_login
+	before_action :require_login
 
 	  def index
-        if params[:category].blank?
-      	@book = Book.all.order("created_at DESC")
-        else
-         @category_id = Category.find_by(name: params[:category]).id
-         @book = Book.where(:category_id => @category_id).order("created_at DESC")
-        end
-      
+		@id = Book.find_by(id: params[:book_id]).id
+		puts ">>>>>>>: #{@id}"
+		@book = Book.where(:id => @id).first
+		puts ">>>>>>>>>>: #{@book.path_read}"
     end
 end
